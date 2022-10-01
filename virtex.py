7bf581c687f3c8adcef7d7dbacc61a908d8185d3
T I M M Y <105170187+Timmydudew@users.noreply.github.com>
2022-10-01 02:09:03 +0100

    Update virtex.py

--- virtex.py
+++ virtex.py
@@ -6,3 +6,14 @@ import random
 import webbrowser
 
 os.clear()
+ logo = "W      H       A       T      S    A    P   P"
+logo2 = "D       e      s       t    r    o   y  e  r"
+
+print(logo)
+print(logo2)
+os.clear()
+print("c r e a t e d    by    T i m m y")
+code = input("pls write the country code")
+First = input("Input the person number to destroy ")
+if + in code :
+   print("Are you  ready  to destroy ")
